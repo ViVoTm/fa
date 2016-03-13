3eb1593168402198b8e9cddaaa7f0bb0dfbb6a4c
ViVoTm <ajaghvajaghemail@gmail.com>
2016-03-13 15:42:26 +0330

    Create feedback.lua

---
+++ plugins/feedback.lua
@@ -0,0 +1,53 @@
+do
+
+ function run(msg, matches)
+
+ 
+
+  local fuse = 'گزارش جدید\n\nآیدی : ' .. msg.from.id .. '\n\nاسم: ' .. msg.from.print_name ..'\n\nیوزرنیم: @' .. msg.from.username .. '\n\nپیام:\n' .. matches[1]  
+
+ local fuses = '!printf user#id' .. msg.from.id
+
+ 
+
+ 
+
+   local text = matches[1]
+
+   local chat = "channel#id"..90312082 
+
+   --like : local chat = "channel#id"..12345678
+
+   
+
+  local sends = send_msg(chat, fuse, ok_cb, false)
+
+  return '✅ پیغام شما ارسال شد ✅'
+
+ 
+
+ end
+
+ end
+
+ return {
+
+  
+
+  description = "گزارش",
+
+ 
+
+  usage = "گزارشی به مدیر",
+
+  patterns = {
+
+  "^گزارش (.*)$"
+
+ 
+
+  },
+
+  run = run
+
+ }
